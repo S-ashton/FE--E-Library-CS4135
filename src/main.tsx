8e1172bc2a3bf8b6d78f1965d9cd33622bf30fb1
S-ashton <sophieannashton@icloud.com>
2026-03-26 17:42:19 +0000

    fix: remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./router.js";
 import './index.css'
-import App from './app/App.jsx' 
 
 
 createRoot(document.getElementById('root')!).render(
